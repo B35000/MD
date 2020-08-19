6d93768ebd577e93a2bd85b502d5ff6f41e94178
bryonyoni <bryonyoni@gmail.com>
2020-08-19 10:59:14 +0300

    updated gitignore file

--- app/src/main/java/com/color/mattdriver/Utilities/ApiStuff.kt
+++ app/src/main/java/com/color/mattdriver/Utilities/ApiStuff.kt
@@ -2,5 +2,5 @@ package com.color.mattdriver.Utilities
 
 class ApiStuff {
     /*the api key for places and geocoder service in gcloud. DO NOT MAKE THESE PUBLIC AND CHANGE PERIODICALLY. The file is in the .gitignore(Project level) btw.*/
-    val geocoder_api_key = "AIzaSyCHtqBJiDiVCpekcSaHq6GpnsHJxX_y8fo"
+    val geocoder_api_key = "AIzaSyDj3_JlJ6GlaedywKOv2awpF4HzhvY0X3w"
 }
